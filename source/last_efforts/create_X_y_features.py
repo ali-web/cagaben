d116059250d0279ae6fe9a6c01f23a3030c75487
LloydM <lloydrmontgomery@gmail.com>
2016-04-03 18:42:59 -0700

    sanity

--- source/last_efforts/create_X_y_features.py
+++ source/last_efforts/create_X_y_features.py
@@ -12,6 +12,29 @@ from sklearn.feature_extraction.text import CountVectorizer
 import numpy as np
 
 
+ADAScores = {
+	'washtimes': 35.4, #1
+	'FoxNews': 39.7, #2
+	'cnn': 56.0, #4
+	'usatoday': 63.4, #6
+	'washingtonpost': 66.6, #8
+	'latimes': 70.0, #9
+	'CBSNews': 73.7, #10
+	'nytimes': 73.7 #11
+}
+
+alignment = {
+	'washtimes': 0,
+	'FoxNews': 0,
+	'cnn': 1,
+	'usatoday': 1,
+	'washingtonpost': 1,
+	'latimes': 1,
+	'CBSNews': 2,
+	'nytimes': 2
+}
+
+
 def pull_mongo_data(db, col):
 
 	# Connect to MongoDB
@@ -27,8 +50,8 @@ def pull_mongo_data(db, col):
 	for source in all_sources:
 
 		# Query the DB for all results that match the given source
-		# db_results = cur.find({ "source" : source }).limit(180)
-		db_results = cur.find({ "source" : source })
+		db_results = cur.find({ "source" : source }).limit(180)
+		# db_results = cur.find({ "source" : source })
 		stories.extend(db_results)
 
 	# Query the DB for all results
@@ -48,10 +71,10 @@ def word_freq(stories):
 	vectorizer = TfidfVectorizer(
 		encoding='latin1', 
 		stop_words='english',
-		# max_features=10000,
+		max_features=2000,
+		use_idf=False,
+		norm=False,
 		binary=True
-		# max_df=.2
-		# min_df=0
 		)
 
 	scipy_word_freq = vectorizer.fit_transform(documents)
@@ -59,9 +82,10 @@ def word_freq(stories):
 	# Get the names of the features (words)
 	f_names = vectorizer.get_feature_names()
 
-	# Convert to Python Generator
+	# Convert to Python List
 	X_train = [np.array(story)[0].tolist() for story in scipy_word_freq.todense()]
 
+	X_train = [[int(freq) for freq in story] for story in X_train]
 
 	return X_train, f_names
 
@@ -75,21 +99,25 @@ def agencies(stories):
 		source = story["source"]
 		if source not in source_lookup:
 			source_lookup[source] = len(source_lookup)
-		y_train.append(source_lookup[source])
+		# y_train.append(source_lookup[source])
+		# y_train.append(ADAScores[source])
+		y_train.append(alignment[source])
 
 	return y_train
 
 
 def write_to_csv(X_train, y_train, f_names):
 
-	with open('story_4000_53000.csv', 'wb') as csvfile:
+	with open('story_400_500_ada_binary.csv', 'wb') as csvfile:
 		wr = csv.writer(
 			csvfile, 
 			delimiter=',', 
 			quoting=csv.QUOTE_MINIMAL)
 
 		# First row is feature names
-		wr.writerow([f.encode('utf8') for f in f_names])
+		row1 = [f.encode('utf8') for f in f_names]
+		row1.append("y_value")
+		wr.writerow(row1)
 
 		for r in range(len(X_train)):
 			X_train[r].append(y_train[r])
@@ -110,9 +138,8 @@ if __name__ == "__main__":
 	# Grab assignments
 	y_train = agencies(db_results)
 
+	# print X_train
 	print len(X_train[0])
-
-	# pprint(f_names)
 
 	# Write to CSV
 	write_to_csv(X_train, y_train, f_names)
